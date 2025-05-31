a2d369299d504df3da5581638dd41ad02217a7f8
xonx4l <sk797119@gmail.com>
2025-05-31 18:35:09 +0530

    add VgaColor

--- onyx-os/src/main.rs
+++ onyx-os/src/main.rs
@@ -27,3 +27,23 @@ pub extern "C" fn kernel_main() -> !{
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
+
+#[allow(dead_code)]
+enum VgaColor {
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGrey = 7,
+    DarkGrey = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    LightMagenta =13,
+    LightBrown = 14,
+    White = 15,
+}
